dbffe839543a66caf0c57430aa737166baf78e74
Esa Nord <esa.nord@cs.tamk.fi>
2018-03-15 19:07:25 +0200

    Initial project commit.

---
+++ TestProject/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _moveSpeed = 5f;
+    [SerializeField]
+    private Transform _playerTransform;
+
+    private CharacterController _charCont;
+    private Vector3 _movement;    
+
+    private void Awake()
+    {
+        _charCont = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {        
+        // Reseting movement vector to prevent sliding.
+        _movement = Vector3.zero;
+
+        _movement = new Vector3(-Input.GetAxis("Vertical"), 0, Input.GetAxis("Horizontal"));
+
+        if (_movement != Vector3.zero)
+        {
+            Vector3 newRotation = _movement;
+            transform.rotation = Quaternion.LookRotation(newRotation);
+            _playerTransform.rotation = Quaternion.LookRotation(newRotation);
+
+            float offsetY = _playerTransform.eulerAngles.y;
+            _playerTransform.eulerAngles = new Vector3(_playerTransform.eulerAngles.x,
+                offsetY + 45, _playerTransform.eulerAngles.z);
+
+            _charCont.Move((transform.forward + transform.right) * _moveSpeed * Time.deltaTime);
+
+            // Move animation.
+        } 
+        
+        else
+        {
+            // Idle animations.
+        }            
+    }
+}
